b021ab0ac0c7e26e702b3db640df86c13c5a3135
renny <rennygarces1983@gmail.com>
2023-04-24 19:25:56 -0300

    javaScript game first version complete

--- script.js
+++ script.js
@@ -2,22 +2,33 @@
 function modifyPage(inputClass, textmessage) {
   document.querySelector(inputClass).textContent = textmessage;
 }
+function modifyInput(inputClass, textmessage) {
+  document.querySelector(inputClass).value = textmessage;
+}
 function modifyValue(inputClass, textmessage) {
   document.querySelector(inputClass).style.width = textmessage;
 }
-
+let scoreUser = 0;
 let opportunities = 10;
 let secretNumber = Math.trunc(Math.random() * 50) + 1;
+let highscore = [0];
 
-console.log(secretNumber + " first secret");
+const calculateSum = (arr) => {
+  return arr.reduce((total, current) => {
+    return total + current;
+  }, 0);
+};
+
 document.querySelector(".again").addEventListener("click", function () {
+  opportunities = 10;
   modifyValue(".progress", "100%");
   secretNumber = Math.trunc(Math.random() * 50) + 1;
-  console.log(secretNumber + " this is the secret");
+
   modifyPage(".attempts", "Attempts 💰 " + 10);
   document.querySelector("body").style.background = "#9f82f8";
   modifyPage(".show-message", "Let's start Again!🤩");
   modifyPage(".secret", "?");
+  modifyInput(".input", "");
 });
 
 document.querySelector(".push").addEventListener("click", function () {
@@ -26,20 +37,25 @@ document.querySelector(".push").addEventListener("click", function () {
   if (!guess || guess < 0) {
     modifyPage(".show-message", "Invalid Number 👎");
   } else if (guess === secretNumber) {
+    highscore.push(opportunities);
+
+    scoreUser = calculateSum(highscore);
+
+    modifyInput(".input", "");
     modifyPage(".attempts", "I'm proud of you! 🙂");
     modifyPage(".show-message", "You're the winner!🏆 🥳");
     modifyPage(".secret", secretNumber);
-    // modifyPage(".highscore", `Highscore: ${highscore}`);
+    modifyPage(".highscore", `Highscore: 💰 ${scoreUser}`);
     document.querySelector("body").style.background = "#51cf66";
   } else if (guess !== secretNumber) {
     guess > secretNumber
-      ? modifyPage(".show-message", "high number!⬇")
-      : modifyPage(".show-message", "low number!⬆");
+      ? modifyPage(".show-message", "high number! 🔼")
+      : modifyPage(".show-message", "low number! 🔽");
     opportunities--;
 
     modifyPage(".attempts", "Attempts 💰" + `Only: ${opportunities}`);
     modifyValue(".progress", `${opportunities * opportunities}%`);
-    console.log(opportunities + " this is oportunities ");
+
     if (opportunities <= 0) {
       modifyPage(".show-message", "You loss the game!❌");
       modifyPage(".attempts", "Play Again!🤾‍♂️");
